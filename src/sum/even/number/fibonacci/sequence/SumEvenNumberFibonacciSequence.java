e5311dd48fe42a73619114ec72c7fc5df3c32b85
Rayzal Ahmad Yaacob <rzlification@gmail.com>
2017-10-06 15:27:56 +0800

    Update SumEvenNumberFibonacciSequence.java

--- src/sum/even/number/fibonacci/sequence/SumEvenNumberFibonacciSequence.java
+++ src/sum/even/number/fibonacci/sequence/SumEvenNumberFibonacciSequence.java
@@ -9,9 +9,6 @@ public class SumEvenNumberFibonacciSequence {
     public static void main(String[] args) {
 	    int x = 1, y = 2, sum, total=2 , limit = 4000000;
             
-           // 0,1,1,2,3,5,8,13,21,34
-            
-            //fn = fn-1 + fn-2
 	    while ((x+y) <= limit)
 	    {
                 sum = x+y;
